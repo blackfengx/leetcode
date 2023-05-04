c9adb0c9b10e52f14723d709d7efdd84363e8de3
blackfengx <blackfengsm@gmail.com>
2023-05-04 17:44:26 -0500

    isValid

---
+++ 01_easy/0020.py
@@ -0,0 +1,27 @@
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+
+# Example 1:
+
+# Input: s = "()"
+# Output: true
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {")": "(", "]": "[", "}": "{"}
+        for c in s:
+            if c in mapping:
+                if not stack or stack.pop() != mapping[c]:
+                    return False
+            else:
+                stack.append(c)
+        return not stack
