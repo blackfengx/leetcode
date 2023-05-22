0acbe97dc86e52477f138e93b031770e246e75c4
blackfengx <blackfengsm@gmail.com>
2023-05-22 09:09:21 -0500

    0028 find index

---
+++ 01_easy/0028.py
@@ -0,0 +1,19 @@
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+# Example 1:
+    # Input: haystack = "sadbutsad", needle = "sad"
+    # Output: 0
+    # Explanation: "sad" occurs at index 0 and 6.
+    # The first occurrence is at index 0, so we return 0.
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle not in haystack:
+            return -1
+
+        haystack.find(needle)
